7a49e1151fddebce4a8b3719541e6151890285de
r0630049 <vanbilloen.3s@hotmail.com>
2017-10-20 16:12:49 +0200

    Startup project for tutorial

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav">
+
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+    <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          view meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>  
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    }
+  }
+
+</script>
